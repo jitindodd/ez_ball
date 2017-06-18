5a3c569b7a7abf03b9330dda27f6f4257d4f3546
jitindodd <jitindodd@gmail.com>
2017-06-18 14:59:05 -0700

    Added layout css, links b/w pages, and other basic stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
